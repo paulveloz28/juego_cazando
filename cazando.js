b94aec83b1fdd49a787be3d7e6a291211cd95502
paulveloz28 <paulveloz28@gmail.com>
2026-04-12 21:05:22 -0500

    feat: implementar movimiento del gato hacia la izquierda

--- cazando.js
+++ cazando.js
@@ -1,9 +1,61 @@
-//obtener canvas
-let canvas = document.getElementById("areaJuego");
-let context = canvas.getContext("2d");
+let canvas=document.getElementById("areaDeJuego");
+let ctx=canvas.getContext("2d");
 
-//Crear una función graficarGato (sin parámetros) que dibuje un rectángulo centrado en el canvas. 
-function graficarGato(){
-    context.fillStyle = "blue";
-    context.fillRect(250,250,25,25);
+//posicion gato
+let gatoX=0;
+let gatoY=0;
+
+//posicion comida
+let comidaX=0;
+let comidaY=0;
+
+//tamaños
+const anchoGato = 80;
+const altoGato = 50;
+const altoComida = 20;
+const anchoComida = 20;  
+
+// -------------------
+
+function limpiarCanvas(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+// -------------------
+
+function graficarGato() {
+    let colorG= "#1900ff";
+    graficarRectangulo(gatoX, gatoY, anchoGato,altoGato, colorG);
+}
+
+function graficarComida() {
+    graficarRectangulo(comidaX,comidaY,anchoComida, altoComida, "#FF0808");
+}
+
+// -------------------
+
+function iniciarJuego() {
+
+    gatoX = (canvas.width / 2) - (anchoGato / 2);
+    gatoY = (canvas.height / 2) - (altoGato / 2);
+
+    graficarGato();
+    graficarComida();
+}
+
+// -------------------
+
+function moverIzquierda(){
+    gatoX = gatoX - 10;
+
+    limpiarCanvas();
+    graficarGato();
+    graficarComida();
+}
+
+// -------------------
+
+function graficarRectangulo(x,y,ancho,alto,color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, ancho, alto);
 }
