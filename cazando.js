2a4787e6f78f1f77edbafe8ff387d4aec72939ea
paulveloz28 <paulveloz28@gmail.com>
2026-04-12 21:49:39 -0500

    feat: mover gato a la derecha, arriba y abajo

--- cazando.js
+++ cazando.js
@@ -1,31 +1,32 @@
 let canvas=document.getElementById("areaDeJuego");
 let ctx=canvas.getContext("2d");
 
-//posicion gato
+// posicion gato
 let gatoX=0;
 let gatoY=0;
 
-//posicion comida
+// posicion comida
 let comidaX=0;
 let comidaY=0;
 
-//tamaños
+// tamaños
 const anchoGato = 80;
 const altoGato = 50;
 const altoComida = 20;
 const anchoComida = 20;  
 
 // -------------------
+// LIMPIAR
 
 function limpiarCanvas(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
 }
 
 // -------------------
+// DIBUJAR
 
 function graficarGato() {
-    let colorG= "#1900ff";
-    graficarRectangulo(gatoX, gatoY, anchoGato,altoGato, colorG);
+    graficarRectangulo(gatoX, gatoY, anchoGato,altoGato, "#1900ff");
 }
 
 function graficarComida() {
@@ -33,9 +34,11 @@ function graficarComida() {
 }
 
 // -------------------
+// INICIAR
 
 function iniciarJuego() {
 
+    // centrar gato solo una vez
     gatoX = (canvas.width / 2) - (anchoGato / 2);
     gatoY = (canvas.height / 2) - (altoGato / 2);
 
@@ -44,6 +47,7 @@ function iniciarJuego() {
 }
 
 // -------------------
+// MOVIMIENTOS
 
 function moverIzquierda(){
     gatoX = gatoX - 10;
@@ -53,7 +57,32 @@ function moverIzquierda(){
     graficarComida();
 }
 
+function moverDerecha(){
+    gatoX = gatoX + 10;
+
+    limpiarCanvas();
+    graficarGato();
+    graficarComida();
+}
+
+function moverArriba(){
+    gatoY = gatoY - 10;
+
+    limpiarCanvas();
+    graficarGato();
+    graficarComida();
+}
+
+function moverAbajo(){
+    gatoY = gatoY + 10;
+
+    limpiarCanvas();
+    graficarGato();
+    graficarComida();
+}
+
 // -------------------
+// UTILIDAD
 
 function graficarRectangulo(x,y,ancho,alto,color){
     ctx.fillStyle = color;
